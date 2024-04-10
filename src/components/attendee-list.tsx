4c389d71cbea2113adeb8a531bc45f25d43889c9
gabriel-lgomes <gabriel_lopes.g@hotmail.com>
2024-04-09 21:15:28 -0300

    feat: pagination created

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,3 +1,4 @@
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
 import {
   Search,
   MoreHorizontal,
@@ -14,13 +15,26 @@ import { TableRow } from "./table/table-row";
 import { formatRelative } from "date-fns/formatRelative";
 import { ptBR } from "date-fns/locale";
 import useFetch from "../hooks/useFetch";
-import { ChangeEvent, useRef, useState } from "react";
+import ReactPaginate from "react-paginate";
 
 export function AttendeeList() {
   const urlAPI = "http://localhost:3000/users";
   const [url, setUrl] = useState(urlAPI);
   const { data } = useFetch(url);
   const search = useRef<HTMLInputElement>(null);
+  const [page, setPage] = useState(0);
+  const [filterData, setFilterData] = useState<any[]>([]);
+  const itemsPerPage = 20;
+
+  useEffect(() => {
+    if (data) {
+      const filtered = data.filter(
+        (item: any, index: number) =>
+          index >= page * itemsPerPage && index < (page + 1) * itemsPerPage
+      );
+      setFilterData(filtered);
+    }
+  }, [data, page]);
 
   function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
     setTimeout(() => {
@@ -63,71 +77,65 @@ export function AttendeeList() {
           </tr>
         </thead>
         <tbody>
-          {data &&
-            data.map((attendees) => (
-              <TableRow
-                key={attendees.id}
-                className="border-b border-white/10 hover:bg-white/5"
-              >
-                <TableCell>
-                  <input
-                    className="bg-black/20 rounded border border-white/18 text-orange-400 outline-none focus:ring-0"
-                    type="checkbox"
-                  />
-                </TableCell>
-                <TableCell>{attendees.id}</TableCell>
-                <TableCell>
-                  <div className="flex flex-col gap-1">
-                    <span className="font-semibold text-zinc-50">
-                      {attendees.name}
-                    </span>
-                    <span>{attendees.email}</span>
-                  </div>
-                </TableCell>
-                <TableCell>
-                  {formatRelative(attendees.createdAt, new Date(), {
-                    locale: ptBR,
-                  })}
-                </TableCell>
-                <TableCell>
-                  {formatRelative(attendees.checkedInAt, new Date(), {
-                    locale: ptBR,
-                  })}
-                </TableCell>
-                <TableCell>
-                  <IconButton transparent={true}>
-                    <MoreHorizontal size={16} />
-                  </IconButton>
-                </TableCell>
-              </TableRow>
-            ))}
-        </tbody>
-        <tfoot>
-          <TableRow>
-            <TableCell colSpan={3}>Mostrando 1 de 200 itens</TableCell>
-            <TableCell className="text-right" colSpan={3}>
-              <div className="inline-flex gap-8 items-center">
-                <span>Página 1 de 10</span>
-
-                <div className="flex gap-1.5">
-                  <IconButton>
-                    <ChevronsLeft size={16} />
-                  </IconButton>
-                  <IconButton>
-                    <ChevronLeft size={16} />
-                  </IconButton>
-                  <IconButton>
-                    <ChevronRight size={16} />
-                  </IconButton>
-                  <IconButton>
-                    <ChevronsRight size={16} />
-                  </IconButton>
+          {filterData.map((attendees) => (
+            <TableRow
+              key={attendees.id}
+              className="border-b border-white/10 hover:bg-white/5"
+            >
+              <TableCell>
+                <input
+                  className="bg-black/20 rounded border border-white/18 text-orange-400 outline-none focus:ring-0"
+                  type="checkbox"
+                />
+              </TableCell>
+              <TableCell>{attendees.id}</TableCell>
+              <TableCell>
+                <div className="flex flex-col gap-1">
+                  <span className="font-semibold text-zinc-50">
+                    {attendees.name}
+                  </span>
+                  <span>{attendees.email}</span>
                 </div>
-              </div>
-            </TableCell>
-          </TableRow>
-        </tfoot>
+              </TableCell>
+              <TableCell>
+                {formatRelative(attendees.createdAt, new Date(), {
+                  locale: ptBR,
+                })}
+              </TableCell>
+              <TableCell>
+                {formatRelative(attendees.checkedInAt, new Date(), {
+                  locale: ptBR,
+                })}
+              </TableCell>
+              <TableCell>
+                <IconButton transparent={true}>
+                  <MoreHorizontal size={16} />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </tbody>
       </Table>
+
+      <ReactPaginate
+        containerClassName={"pagination"}
+        pageClassName={"page-item"}
+        activeClassName={"active text-[#f48f56] font-bold"}
+        className="flex items-center justify-end gap-4"
+        onPageChange={(event) => setPage(event.selected)}
+        pageCount={Math.ceil((data?.length ?? 0) / itemsPerPage)}
+        breakLabel="..."
+        previousLabel={
+          <IconButton>
+            <ChevronsLeft size={16} />
+          </IconButton>
+        }
+        nextLabel={
+          <IconButton>
+            <ChevronsRight size={16} />
+          </IconButton>
+        }
+      />
     </div>
   );
 }
